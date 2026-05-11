70c518df99562b07e19e86a9c79ded362501c402
Ameeq Ul Haq <ameeq.haq.contact@gmail.com>
2026-05-11 22:19:49 +0500

    praticing

--- FlaskApi/integrating.py
+++ FlaskApi/integrating.py
@@ -1,17 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
-
-@app.route('/')
 def welcome():
     return render_template('index.html')
 
-@app.route('/MEM/<int:marks>')
+app.add_url_rule('/', 'welcome', welcome)
+
+
+@app.route('/succes/<int:marks>')
+def success(marks):
+    return "you are passed " + str(marks)
+
+@app.route('/fail/<int:marks>')
+def fail(marks):
+    return "you are failed " + str(marks)
+
+@app.route('/result/<int:marks>')
 def mark(marks):
     if marks < 50: 
-        return 'fail'
+        return redirect(url_for('fail', marks= marks))
     else: 
-        return 'pass'
-
+        return redirect(url_for('success', marks= marks))
+    
 if __name__ == '__main__':
     app.run(debug=True) 
